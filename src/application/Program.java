ce8de6c4cf4ed9c73bd91c149755fd9634f34af4
PedroLeonardo8 <pedroleonardo841@gmail.com>
2020-07-30 11:43:20 -0300

    Solução melhor que o primeiro commit, mas ainda ruim

--- src/application/Program.java
+++ src/application/Program.java
@@ -34,17 +34,15 @@ public class Program {
 			System.out.print("Check-out:(dd/MM/yyyy) ");
 			checkout = sdf.parse(sc.next());
 
-			Date now = new Date();
-			if (checkin.before(now) || checkout.before(now)) {
-				System.out.println("Error in reservation: Check-out date must be after check-in date.");
-			} else if (!checkout.after(checkin)) {
-				System.out.println("Error in reservation: Check-out date must be after check-in date.");
-
-			} else {
-				reservation.updateDates(checkin, checkout);
+			String error = reservation.updateDates(checkin, checkout);
+			if(error!=null) {
+				System.out.println(error);
+				}
+			
+			else{
 				System.out.println(reservation);
 			}
-
+			
 		}
 
 		sc.close();
